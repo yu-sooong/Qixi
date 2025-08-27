b6b9383308ab0336e2512e6329adfddeb7e4a231
Ting Yu <ting.yu@ubitech.com.tw>
2025-08-27 17:29:57 +0800

    add meta tags photo

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,20 @@ import vue from '@vitejs/plugin-vue'
 
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
 const isCI = process.env.GITHUB_ACTIONS === 'true'
+const baseUrl = isCI ? `/${repo}/` : '/'
+const siteUrl = isCI ? `https://ting.github.io/${repo}/` : 'http://localhost:5173/'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: isCI ? `/${repo}/` : '/',
+  plugins: [
+    vue(),
+    {
+      name: 'replace-meta-tags',
+      transformIndexHtml(html) {
+        return html
+          .replace(/%VITE_BASE_URL%/g, siteUrl)
+          .replace(/%VITE_SITE_URL%/g, siteUrl)
+      }
+    }
+  ],
+  base: baseUrl,
 })
